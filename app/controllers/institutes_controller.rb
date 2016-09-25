0a3a10320418b475388cf089cf7f0129f5c2669b
Vishal Sharma <vish.sharma0203@gmail.com>
2016-09-26 01:45:39 +0530

    refactored institute creation code and removed unused code for users

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -2,9 +2,9 @@ class InstitutesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    institute = Institute.create(user_id: current_user[:id], name: institute_params[:name],
-                                 description: institute_params[:description])
-    render :json => institute
+    institute = Institute.new(institute_params)
+    current_user.institutes << institute
+    render :json => { institute: institute }
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,5 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def new
-  end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      render json: user, status: 201
-    else
-      render json: {error: 'Invalid request'}, status: 400
-    end
-  end
-
-  private
-    def user_params
-      params.require(:user)
-          .permit(:first_name, :last_name, :email, :password, :password_confirmation, :occupation)
-    end
 end
